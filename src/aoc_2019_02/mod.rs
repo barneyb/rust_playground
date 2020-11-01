aefb3c9103bcd2bc7b68ddd1d25cda0ea5691868
Barney Boisvert <bboisvert@gmail.com>
2020-11-01 13:18:44 -0800

    refactor for part two of 2019 day 2

--- src/aoc_2019_02/mod.rs
+++ src/aoc_2019_02/mod.rs
@@ -5,11 +5,27 @@ mod machine;
 
 pub fn run() {
     let filename = cli::aoc_filename("aoc_2019_02.txt");
-    let mut prog = read_program(filename);
-    prog[1] = 12;
-    prog[2] = 2;
+    let prog = read_program(filename);
+    println!("Part One: {}", eval_noun_verb(&prog, 12, 2));
+
+    let mut result: i32 = -1;
+    'outer: for n in 0..=99 {
+        for v in 0..=99 {
+            if eval_noun_verb(&prog, n, v) == 19690720 {
+                result = n * 100 + v;
+                break 'outer;
+            }
+        }
+    };
+    println!("Part Two: {}", result);
+}
+
+fn eval_noun_verb(prog: &Vec<i32>, noun: i32, verb: i32) -> i32 {
+    let mut prog = prog.clone();
+    prog[1] = noun;
+    prog[2] = verb;
     machine::run(&mut prog);
-    println!("Result: {}", prog[0]);
+    prog[0]
 }
 
 fn read_program(filename: String) -> Vec<i32> {
