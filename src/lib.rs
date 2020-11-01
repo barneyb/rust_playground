a6a841e14529633d1dd86a1b95de5d2761f6057f
Barney Boisvert <bboisvert@gmail.com>
2020-11-01 08:40:45 -0800

    use a CLI menu for selecting utilties to run, dynamically

---
+++ src/lib.rs
@@ -0,0 +1,41 @@
+use std::collections::HashMap;
+
+mod guess_number;
+mod temp_convert;
+
+pub struct Utilities<'a> {
+    utilities: HashMap<&'a str, fn() -> ()>,
+    names: Vec<&'a str>,
+}
+
+impl<'a> Utilities<'a> {
+
+    pub fn new() -> Utilities<'a> {
+        let mut utilities: HashMap<&str, fn() -> ()> = HashMap::new();
+        // todo: "add Sally to Engineering" / "show Engineering" interface
+        // todo: convert strings to pig latin
+        // todo: mean/median/mode of list of integers
+        // todo: 12 days of christmas
+        // todo: fibonacci
+        utilities.insert("temp", temp_convert::run);
+        utilities.insert("guess", guess_number::run);
+        let mut names: Vec<&str> = utilities.iter()
+            .map(|(k, _)| *k)
+            .collect();
+        names.sort_unstable();
+        Utilities {
+            utilities,
+            names,
+        }
+    }
+
+    pub fn names(&self) -> &Vec<&str> {
+        &self.names
+    }
+
+    pub fn by_name(&self, name: &str) -> Option<&fn()> {
+        self.utilities.get(name)
+    }
+
+}
+
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,36 @@
-mod guess_number;
-mod temp_convert;
+use rust_playground::Utilities;
+use std::io;
 
 fn main() {
-    // todo: 12 days of christmas
-    // todo: fibonacci
-    temp_convert::run();
-    guess_number::run();
+    let utils = Utilities::new();
+    'outer: loop {
+        println!("Available utilities:");
+        for (i, n) in utils.names().iter().enumerate() {
+            println!(" {:2}) {}", i + 1, n)
+        }
+        loop {
+            println!("Select a module (name or number, 'q' to quit): ");
+
+            let mut input = String::new();
+            match io::stdin().read_line(&mut input) {
+                Err(_) => continue,
+                Ok(2) if "q\n" == input => break 'outer,
+                Ok(_) => {
+                    let mut nori = input.trim();
+                    if let Ok(i) = nori.parse::<usize>() {
+                        if let Some(n) = utils.names().get(i - 1) {
+                            nori = n;
+                        }
+                    }
+                    if let Some(f) = utils.by_name(&nori) {
+                        f();
+                        break
+                    } else {
+                        println!("No '{}' utility is known", nori);
+                        continue
+                    }
+                }
+            }
+        }
+    }
 }
