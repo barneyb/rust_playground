b37248086675dfcda93c45873eca4b6d08c8a594
Barney Boisvert <bboisvert@gmail.com>
2020-11-01 09:28:58 -0800

    allow specifying a utility name on the command line

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,17 @@
 use rust_playground::Utilities;
-use std::io;
+use std::{io, env};
 
 fn main() {
+    let mut args = env::args();
+    args.next(); // kill command name
+
     let utils = Utilities::new();
+
+    if let Some(n) = args.next() {
+        if let AndThen::Exit() = handle_input(&utils, &n) {
+            return;
+        }
+    }
     println!("Available utilities:");
     for (i, n) in utils.names().iter().enumerate() {
         println!(" {:2}) {}", i + 1, n)
@@ -12,22 +21,34 @@ fn main() {
         let mut input = String::new();
         match io::stdin().read_line(&mut input) {
             Err(_) => continue,
-            Ok(2) if "q\n" == input => break,
-            Ok(_) => {
-                let mut nori = input.trim();
-                if let Ok(i) = nori.parse::<usize>() {
-                    if let Some(n) = utils.names().get(i - 1) {
-                        nori = n;
-                    }
-                }
-                if let Some(f) = utils.by_name(&nori) {
-                    f();
-                    break
-                } else {
-                    println!("No '{}' utility is known", nori);
-                    continue
-                }
-            }
+            Ok(_) => match handle_input(&utils, &input) {
+                AndThen::Next() => continue,
+                AndThen::Exit() => return,
+            },
         }
     }
+}
+
+fn handle_input(utils: &Utilities, input: &String) -> AndThen {
+    if "q\n" == input {
+        return AndThen::Exit()
+    }
+    let mut nori = input.trim();
+    if let Ok(i) = nori.parse::<usize>() {
+        if let Some(n) = utils.names().get(i - 1) {
+            nori = n;
+        }
+    }
+    if let Some(f) = utils.by_name(&nori) {
+        f();
+        AndThen::Exit()
+    } else {
+        println!("No '{}' utility is known", nori);
+        AndThen::Next()
+    }
+}
+
+enum AndThen {
+    Next(),
+    Exit(),
 }
