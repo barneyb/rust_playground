8ead416a13bd0a112e9cf7fe2531b953aeccdaee
Barney Boisvert <bboisvert@gmail.com>
2020-11-07 10:13:23 -0800

    expose a runnable intcode Machine type, rather than just run()

--- src/aoc_2019_02/mod.rs
+++ src/aoc_2019_02/mod.rs
@@ -23,7 +23,7 @@ fn eval_noun_verb(prog: &Vec<i32>, noun: i32, verb: i32) -> i32 {
     let mut prog = prog.clone();
     prog[1] = noun;
     prog[2] = verb;
-    intcode::run(&mut prog);
+    intcode::Machine::new(&mut prog).run();
     prog[0]
 }
 
--- src/intcode/mod.rs
+++ src/intcode/mod.rs
@@ -1,23 +1,22 @@
 use std::ops::{Add, Mul};
 
-pub fn run(program: &mut Vec<i32>) {
-    let mut ex = Execution::new(program);
-    while !ex.halted() {
-        ex.step();
-    }
-}
-
-struct Execution<'a> {
+pub struct Machine<'a> {
     ip: usize,
     program: &'a mut Vec<i32>,
 }
 
-impl<'a> Execution<'a> {
+impl<'a> Machine<'a> {
 
-    fn new(program: &mut Vec<i32>) -> Execution {
-        Execution {
+    pub fn new(program: &mut Vec<i32>) -> Machine {
+        Machine {
             ip: 0,
             program,
+        }
+    }
+
+    pub fn run(&mut self) {
+        while !self.halted() {
+            self.step();
         }
     }
 
@@ -61,7 +60,7 @@ mod tests {
     #[test]
     fn part_one_discussion() {
         let mut prog = vec![1,9,10,3,2,3,11,0,99,30,40,50];
-        run(&mut prog);
+        Machine::new(&mut prog).run();
         assert_eq!(prog[3], 70);
         assert_eq!(prog[0], 3500);
     }
@@ -69,28 +68,28 @@ mod tests {
     #[test]
     fn part_one_example_one() {
         let mut prog = vec![1,0,0,0,99];
-        run(&mut prog);
+        Machine::new(&mut prog).run();
         assert_eq!(prog[0], 2);
     }
 
     #[test]
     fn part_one_example_two() {
         let mut prog = vec![2,3,0,3,99];
-        run(&mut prog);
+        Machine::new(&mut prog).run();
         assert_eq!(prog[3], 6);
     }
 
     #[test]
     fn part_one_example_three() {
         let mut prog = vec![2,4,4,5,99,0];
-        run(&mut prog);
+        Machine::new(&mut prog).run();
         assert_eq!(prog[5], 9801);
     }
 
     #[test]
     fn part_one_example_four() {
         let mut prog = vec![1,1,1,4,99,5,6,0,99];
-        run(&mut prog);
+        Machine::new(&mut prog).run();
         assert_eq!(prog[0], 30);
         assert_eq!(prog[4], 2);
     }
