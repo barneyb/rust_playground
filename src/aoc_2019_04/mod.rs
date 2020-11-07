f9cfb522742267b1962c1338fd2db42796a54c53
Barney Boisvert <bboisvert@gmail.com>
2020-11-07 08:57:37 -0800

    split out a test helper

--- src/aoc_2019_04/mod.rs
+++ src/aoc_2019_04/mod.rs
@@ -5,31 +5,51 @@ pub fn run() {
     let (mut curr, end) = parse();
     println!(" from {}", curr);
     println!("   to {}", end);
+    println!("range {}", end - curr + 1);
     let mut count = 0;
     let mut iterations = 0;
-    'outer: while curr <= end {
+    while curr <= end {
         iterations += 1;
-        let mut last = curr % 10;
-        let mut rest = curr / 10;
-        let mut mult = 1;
-        let mut double = false;
-        while rest > 0 {
-            let next = rest % 10;
-            if next > last {
-                curr += (next - last) * mult;
-                continue 'outer
-            }
-            rest /= 10;
-            mult *= 10;
-            double = double || next == last;
-            last = next;
-        }
-        if double {
+        let r = test(curr);
+        if r.accept {
             count += 1;
         }
-        curr += 1;
+        curr += r.skip;
     }
     println!("count {} in {} iterations", count, iterations);
+}
+
+struct Result {
+    accept: bool,
+    skip: i32,
+}
+
+fn test(n: i32) -> Result {
+    let mut last = n % 10;
+    let mut rest = n / 10;
+    let mut place = 1;
+    let mut accept = false;
+    while rest > 0 {
+        let curr = rest % 10;
+        // if we're ascending, skip to the next possibility
+        if curr > last {
+            return Result {
+                accept: false,
+                skip: (curr - last) * place,
+            }
+        }
+        rest /= 10;
+        place *= 10;
+        // check for a run
+        if curr == last {
+            accept = true;
+        }
+        last = curr;
+    }
+    return Result {
+        accept,
+        skip: 1,
+    }
 }
 
 fn parse() -> (i32, i32) {
