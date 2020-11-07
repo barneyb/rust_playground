4a53429bfd0a4e84f5d3fb638dda207042ca7512
Barney Boisvert <bboisvert@gmail.com>
2020-11-07 10:48:31 -0800

    set up IO for Intcode

--- src/intcode/mod.rs
+++ src/intcode/mod.rs
@@ -1,8 +1,35 @@
 use std::ops::{Add, Mul};
 
+struct Input<'a> {
+    buffer: Option<&'a mut Vec<i32>>,
+}
+
+impl<'a> Input<'a> {
+    fn read(&mut self) -> i32 {
+        match &mut self.buffer {
+            Some(b) => b.remove(0),
+            None => panic!("No input is available!")
+        }
+    }
+}
+
+struct Output<'a> {
+    buffer: Option<&'a mut Vec<i32>>,
+}
+
+impl<'a> Output<'a> {
+    fn write(&mut self, n: i32) {
+        if let Some(b) = &mut self.buffer {
+            b.push(n)
+        }
+    }
+}
+
 pub struct Machine<'a> {
     ip: usize,
     program: &'a mut Vec<i32>,
+    stdin: Input<'a>,
+    stdout: Output<'a>,
 }
 
 impl<'a> Machine<'a> {
@@ -11,7 +38,17 @@ impl<'a> Machine<'a> {
         Machine {
             ip: 0,
             program,
+            stdin: Input { buffer: None },
+            stdout: Output { buffer: None },
         }
+    }
+
+    pub fn stdin(&mut self, buffer: &'a mut Vec<i32>) {
+        self.stdin = Input { buffer: Some(buffer) }
+    }
+
+    pub fn stdout(&mut self, buffer: &'a mut Vec<i32>) {
+        self.stdout = Output { buffer: Some(buffer) }
     }
 
     pub fn run(&mut self) {
@@ -27,6 +64,14 @@ impl<'a> Machine<'a> {
         match self.next() {
             1 => self.binary_op(i32::add),
             2 => self.binary_op(i32::mul),
+            3 => {
+                let pos = self.next_pos();
+                self.program[pos] = self.stdin.read()
+            },
+            4 => {
+                let pos = self.next_pos();
+                self.stdout.write(self.program[pos])
+            },
             99 => self.ip = usize::max_value(),
             opcode => panic!("Unknown opcode {} (at position {})", opcode, self.ip),
         };
@@ -38,12 +83,16 @@ impl<'a> Machine<'a> {
         v
     }
 
+    fn next_pos(&mut self) -> usize {
+        self.next() as usize
+    }
+
     fn binary_op<F>(&mut self, mut f: F)
         where F: FnMut(i32, i32) -> i32
     {
-        let a = self.next() as usize;
-        let b = self.next() as usize;
-        let c = self.next() as usize;
+        let a = self.next_pos();
+        let b = self.next_pos();
+        let c = self.next_pos();
         self.program[c] = f(self.program[a], self.program[b]);
     }
 
@@ -92,6 +141,20 @@ mod tests {
         Machine::new(&mut prog).run();
         assert_eq!(prog[0], 30);
         assert_eq!(prog[4], 2);
+    }
+
+    #[test]
+    fn basic_io() {
+        let mut prog = vec![3,0,4,0,99];
+        let mut m = Machine::new(&mut prog);
+        let mut input = vec![42, 1, 2, 3];
+        let mut output = vec![4, 5, 6];
+        m.stdin(&mut input);
+        m.stdout(&mut output);
+        m.run();
+        assert_eq!(prog[0], 42); // the temp storage
+        assert_eq!(input[0], 1); // read from the head
+        assert_eq!(output[3], 42); // wrote to the tail
     }
 
 }
