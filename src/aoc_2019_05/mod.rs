cef17c8dd592263c5db1f6599da32c1b1f57acd9
Barney Boisvert <bboisvert@gmail.com>
2020-11-11 07:27:15 -0800

    part two AoC 2019 day 5

--- src/aoc_2019_05/mod.rs
+++ src/aoc_2019_05/mod.rs
@@ -3,13 +3,22 @@ use crate::intcode;
 
 pub fn run() {
     let filename = cli::aoc_filename("aoc_2019_05.txt");
-    let prog = intcode::read_from_file(filename);
-    let mut prog = prog.clone();
+    let orig_prog = intcode::read_from_file(filename);
+    let mut prog = orig_prog.clone();
     let mut m = intcode::Machine::new(&mut prog);
     let mut input = vec![1];
     m.stdin(&mut input);
     let mut output = vec![];
     m.stdout(&mut output);
     m.run();
-    println!("{:?}", output)
+    println!("{:?}", output);
+
+    let mut prog = orig_prog.clone();
+    let mut m = intcode::Machine::new(&mut prog);
+    let mut input = vec![5];
+    m.stdin(&mut input);
+    let mut output = vec![];
+    m.stdout(&mut output);
+    m.run();
+    println!("{:?}", output);
 }
