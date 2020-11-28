72663cc85038fda74f51739485db8a35f422be30
Barney Boisvert <bboisvert@gmail.com>
2020-11-28 09:08:53 -0800

    use a thread for the machine and do the IO myself

--- src/aoc_2019_13/mod.rs
+++ src/aoc_2019_13/mod.rs
@@ -1,10 +1,25 @@
+use std::any::Any;
+use std::sync::mpsc;
+use std::thread;
+use std::time::Duration;
+
 use crate::cli;
 use crate::intcode;
 
 pub fn run() {
     let filename = cli::aoc_filename("aoc_2019_13.txt");
-    let prog = intcode::read_from_file(filename);
-    let output = intcode::one_off_output(&prog, None);
+    let mut prog = intcode::read_from_file(filename);
+    let (tx, stdout) = mpsc::channel();
+    let thread = thread::spawn(move || {
+        let mut m = intcode::Machine::new(&prog);
+        m.with_stdout(tx);
+        m.run();
+    });
+    // now read!
+    let mut output = Vec::new();
+    for v in stdout {
+        output.push(v)
+    }
     let mut count = 0;
     for i in (2..output.len()).step_by(3) {
         if output[i] == 2 {
@@ -12,4 +27,5 @@ pub fn run() {
         }
     }
     println!("{}", count);
+    thread.join().expect("Failed to join thread");
 }
